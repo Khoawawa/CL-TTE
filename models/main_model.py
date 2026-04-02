0fb5acb6245def5652ef43a3bb6d84931b6965f7
Khoawawa <atom08072004@gmail.com>
2026-04-03 00:46:46 +0700

    a

--- models/main_model.py
+++ models/main_model.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 from models.blocks.encoder import SegmentEncoder
 from models.blocks.LayerNormGRU import LayerNormBiGRU
 from models.loss.contrastive_loss import SoftContrastiveLoss
-
+from models.base.PositionalEncoding import PositionalEncodingIndex
 class Cl_TTE(nn.Module):
     def __init__(self, d_model, nhead, seq_layer,temperature=0.1, sigma_percent=0.1):
         super().__init__()
@@ -12,6 +12,8 @@ class Cl_TTE(nn.Module):
         self.temperature = temperature
         # segment encoding
         self.enc = SegmentEncoder(d_model)
+        
+        self.positional_encoder = PositionalEncodingIndex(d_model=d_model)
         # map
         encoder_layer = nn.TransformerEncoderLayer(d_model=d_model, nhead=nhead, batch_first=True, norm_first=True)
         self.mapper = nn.TransformerEncoder(encoder_layer, num_layers=seq_layer)
@@ -32,10 +34,10 @@ class Cl_TTE(nn.Module):
         self.cls_token = nn.Parameter(torch.randn(1,1,d_model))
         
         self.contrastive_mlp = nn.Sequential(
-            nn.Linear(d_model, d_model//2),
-            nn.BatchNorm1d(d_model//2),
-            nn.LeakyReLU(),
-            nn.Linear(d_model//2, d_model//4)
+            nn.Linear(d_model, d_model),
+            nn.BatchNorm1d(d_model),
+            nn.ReLU(),
+            nn.Linear(d_model, d_model//2)
         )
         
         self.contrastive_loss = SoftContrastiveLoss(temperature=temperature, sigma_percent=sigma_percent, distance_metric='relative')
@@ -49,8 +51,8 @@ class Cl_TTE(nn.Module):
         count = mask.sum(dim=1).unsqueeze(-1)  # (B, 1)
         mean_h = sum_h / count.clamp(min=1)  # (B, D)
         return mean_h
-    def contrastive_branch(self, h, y_true, segment_mask):
-        z = self.mean_pooling(h, segment_mask)
+    def contrastive_branch(self, h, y_true):
+        z = h[:, 0, :] # (B, D) CLS token representation
         z_proj = F.normalize(self.contrastive_mlp(z), dim=-1) # (B, D//4)
         with torch.amp.autocast('cuda',enabled=False):
             loss = self.contrastive_loss(z_proj.float(), y_true.float())
@@ -73,29 +75,37 @@ class Cl_TTE(nn.Module):
         
         segment_rep, datetimerep = self.enc(links,dateinfo)  # (B, T, D)
         
-        B, T, D = segment_rep.shape
+        pos = self.positional_encoder(segment_rep, padding_mask=padding_mask) # (B, T, D)
+        segment_rep = segment_rep + pos
+        # summarize token
+        B, T_plus_one, D = segment_rep.shape
+        cls_tokens = self.cls_token.expand(B, -1, -1)  # (B, 1, D)
+        segment_rep = torch.cat([cls_tokens, segment_rep], dim=1)  # (B, T+1, D)
         
+        padding_mask_with_cls = torch.cat([torch.zeros(B, 1, dtype=torch.bool, device=padding_mask.device), padding_mask], dim=1)  # (B, T+1)
         # LEARNING THE MAP OF THE TRAJECTORY
-        map_memo = self.mapper(segment_rep, src_key_padding_mask=padding_mask) # (B, T, D)
-        
+        map_memo = self.mapper(segment_rep, src_key_padding_mask=padding_mask_with_cls) # (B, T, D)
+
         # contrastive learning branch
         if self.training:
-            l_cl = self.contrastive_branch(map_memo, y_true, segment_mask)
+            l_cl = self.contrastive_branch(map_memo, y_true)
         else:
             l_cl = None
         # regression branch
         # CAUSAL TRASNFORMER DECODER ACT AS ANTICIPATOR (DRIVER)
-        causal_mask = nn.Transformer.generate_square_subsequent_mask(T, device=links.device) # (T, T)
         
+        T = T_plus_one - 1
+        causal_mask = nn.Transformer.generate_square_subsequent_mask(T, device=links.device) # (T, T)
+
         driver_states = self.anticipator(
-            tgt = segment_rep,
-            memory = map_memo,
+            tgt = segment_rep[:, 1:, :], # (B, T, D) exclude CLS token
+            memory = map_memo, # (B, T, D) exclude CLS token
             tgt_mask = causal_mask,
             tgt_key_padding_mask = padding_mask,
-            memory_key_padding_mask = padding_mask
+            memory_key_padding_mask = padding_mask_with_cls
         )
         
-        last_step_indices = (lens - 1).clamp(min=0)
+        last_step_indices = (lens - 1).clamp(min=0)  # (B,) ensure non-negative
         gather_indices = last_step_indices.view(B, 1, 1).expand(B, 1, self.d_model)  # (B, 1, D)
         
         h_final = driver_states.gather(1, index=gather_indices).squeeze(1)  # (B, D)
