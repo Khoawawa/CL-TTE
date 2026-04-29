d5604b3582e21b9d15c53f905cc412f878535348
Khoawawa <atom08072004@gmail.com>
2026-04-29 11:11:28 +0700

    a

--- models/blocks/encoder.py
+++ models/blocks/encoder.py
@@ -10,12 +10,12 @@ from models.blocks.cl import MSM
 from models.blocks.poi import PoiEncoder, GlobalFiLM
 
 from models.profiler.profiler import BlockTimer
-
 class ContrastiveEncoder(nn.Module):
-    def __init__(self, d_model, nhead, r_seconds=45, dropout=0.1, nlayer=4, contrastive_temperature=0.25):
+    def __init__(self, d_model, nhead, r_seconds=45, dropout=0.1, nlayer=4, contrastive_temperature=0.25, recon_weight=0.1):
         super().__init__()
         self.r_seconds = r_seconds
         self.contrastive_temperature = contrastive_temperature
+        self.recon_weight = recon_weight
 
         self.msm = MSM(d_model,nhead,dropout,nlayer)
         
@@ -25,6 +25,7 @@ class ContrastiveEncoder(nn.Module):
             nn.Linear(d_model * 2, d_model)
         )
         self.loss = HardContrastiveLoss(temperature=self.contrastive_temperature)
+        self.recon_loss = ReconstructionLoss(d_model)
         
     def create_pos_mask(self, y_true):
         # y_true: (B, T)
@@ -87,23 +88,30 @@ class ContrastiveEncoder(nn.Module):
             augment_pad_mask = src_key_augment_padding_mask
         # encode
         if self.training:
-            x_all = torch.cat([x, x_aug], dim=0)
-            pad_mask_all = torch.cat([pad_mask, augment_pad_mask], dim=0)
+            x_masked, mask_positions = self.recon_loss.apply_mask(x, pad_mask)
             
-            h_all = self.msm(x_all, pad_mask_all)
-            z_all = self.masked_mean_pooling(h_all, pad_mask_all)
+            x_all = torch.cat([x_masked,x_aug],dim=0) # (2B, T, D)
+            pad_mask_all = torch.cat([pad_mask,augment_pad_mask],dim=0) # (2B, T)
+            
+            # MSM handle PE
+            h_all = self.msm(x_all,pad_mask_all)
+            
+            z_all = self.masked_mean_pooling(h_all,pad_mask_all) # (2B, D)
             
             pos_mask = self.create_pos_mask(y_true)
-            z_all_proj = self.proj(z_all)
+            z_all_proj = F.normalize(self.proj(z_all), dim=-1)
+            # contrastive learning
             l_cl = self.loss(z_all_proj, pos_mask)
             
-            h_msm = h_all[:B]
+            h_msm_masked = h_all[:B]
+            l_recon = self.recon_loss(h_msm_masked, x, mask_positions)
+            l_combined = l_cl + self.recon_weight * l_recon
             
-            l_combined = l_cl            
+            h_msm = self.msm(x, pad_mask)
         else:
             h_msm = self.msm(x,pad_mask)
             
-            l_combined = None
+            l_combined = None 
         
         return h_msm, l_combined
     
