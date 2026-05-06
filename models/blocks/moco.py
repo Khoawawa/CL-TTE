be77a840858e44f5cc1b7ea9b2cb7869f7f1918a
Khoawawa <atom08072004@gmail.com>
2026-05-06 12:39:00 +0700

    a

--- models/blocks/moco.py
+++ models/blocks/moco.py
@@ -167,8 +167,7 @@ class MoCo(nn.Module):
         
         alpha = min(1,(np.exp(epoch/max_epoch) - 1) / (np.e - 1))
         # normalize to keep scale bounded regardless of queue size
-        soft_sum = soft_weights.sum(dim=1, keepdim=True).clamp(min=1)
-        l_neg = -(soft_weights / soft_sum * log_probs[:, 1:]).sum(dim=1)
+        l_neg = -(soft_weights * log_probs[:, 1:]).sum(dim=1) / self.queue_size
         
         return (l_pos + alpha * l_neg).mean()
 
