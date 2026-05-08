be21ca6e5d6cbdf2b9e04d5a50aef11cf47357f1
Khoawawa <atom08072004@gmail.com>
2026-05-08 17:52:13 +0700

    a

--- models/main_model.py
+++ models/main_model.py
@@ -86,8 +86,8 @@ class Cl_TTE(nn.Module):
         
         # TEMPORAL ENCODING
         if profiler: profiler.start('GRU')
-        h = segment_rep_clean + self.ctx_proj_to_seq(h_msm)
-        h = self.film(h, datetimerep)
+        h = self.film(segment_rep_clean, datetimerep)
+        h = h + self.ctx_proj_to_seq(h_msm)
         h = h.transpose(0,1).contiguous() # (T, B, D)
         h,_ = self.temporal_block(h, lens) # (B, T, D)
         h = h.transpose(0,1).contiguous()
